84d44f8be4f559f6b151e1eeb6a1954504603131
pikachu-78 <divyanjalisarabu@gmail.com>
2023-05-22 21:46:01 +0530

    first commit

---
+++ app/components/modals/RegisterModal.tsx
@@ -0,0 +1,129 @@
+'use client'
+
+import axios from 'axios'
+import { AiFillGithub } from 'react-icons/ai'
+import { FcGoogle } from 'react-icons/fc'
+import { useCallback, useState } from 'react'
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
+import useRegisterModal from '@/app/hooks/useRegisterModal'
+import Modal from './Modal'
+import Heading from '../Heading'
+import Input from '../inputs/Input'
+import { toast } from 'react-hot-toast'
+import Button from '../Button'
+import { signIn } from 'next-auth/react'
+
+const RegisterModal = () => {
+  const registerModal = useRegisterModal()
+  const [isLoading, setIsLoading] = useState(false)
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: '',
+      email: '',
+      password: ''
+    }
+  })
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true)
+
+    axios
+      .post('/api/register', data)
+      .then(() => {
+        toast.success('Registered!')
+        registerModal.onClose()
+      })
+      .catch((error) => {
+        toast.error('Something went wrong .')
+      })
+      .finally(() => {
+        setIsLoading(false)
+      })
+  }
+
+  const toggle = useCallback(() => {
+    registerModal.onClose()
+  }, [registerModal])
+
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Heading
+        title="Welcome to Airbnb"
+        subtitle="Create an account!"
+      />
+      <Input
+        id="email"
+        label="Email"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="name"
+        label="Name"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="password"
+        label="Password"
+        type="password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </div>
+  )
+
+  const footerContent = (
+    <div className="mt-3 flex flex-col gap-4">
+      <hr />
+      <Button
+        outline
+        label="Continue with Google"
+        icon={FcGoogle}
+        onClick={() => signIn('google')}
+      />
+      <Button
+        outline
+        label="Continue with Github"
+        icon={AiFillGithub}
+        onClick={() => signIn('github')}
+      />
+      <div className="mt-4 text-center font-light text-neutral-500">
+        <div className="flex flex-row items-center justify-center gap-2">
+          <div>Already have an account?</div>
+          <div
+            onClick={toggle}
+            className="cursor-pointer text-neutral-800 hover:underline">
+            Login
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+
+  return (
+    <Modal
+      disabled={isLoading}
+      isOpen={registerModal.isOpen}
+      title="Register"
+      actionLabel="Continue"
+      onClose={registerModal.onClose}
+      onSubmit={handleSubmit(onSubmit)}
+      body={bodyContent}
+      footer={footerContent}
+    />
+  )
+}
+
+export default RegisterModal
---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import { Nunito } from "next/font/google";
+import './globals.css'
+import Navbar from "./components/navbar/Navbar";
+import ClientOnly from './components/ClientOnly';
+import RegisterModal from '@/app/components/modals/RegisterModal';
+import ToasterProvider from "./providers/ToasterProvider";
+
+export const metadata = {
+  title: 'Airbnb',
+  description: 'Airbnb clone',
+}
+
+const font = Nunito({
+  subsets: ["latin"],
+})
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ClientOnly>
+          <ToasterProvider />
+          <RegisterModal />
+          <Navbar />
+        </ClientOnly>
+        {children}
+      </body>
+    </html>
+  )
+}
